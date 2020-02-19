809c0faa84d8e82eddd1f70415160f8ebeffab6a
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-02-19 19:53:09 +0300

    patch_07, 19:53

--- modular-rails/example/rails/spec/features/open_root_spec.rb
+++ modular-rails/example/rails/spec/features/open_root_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "OpenRoots", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "open home page and see something" do
+    visit "/"
+  end
 end
