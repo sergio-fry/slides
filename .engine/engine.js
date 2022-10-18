ec78d0661cae81ef78e2af6aa72946d94763361e
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-10-18 17:56:51 +0300

    engine update

--- .engine/engine.js
+++ .engine/engine.js
@@ -3,12 +3,7 @@ const { Marp } = require('@marp-team/marp-core')
 module.exports = function(opts) {
   opts['html'] = true;
 
-  plantUmlOptions = {
-    openMarker: '```plantuml',
-    closeMarker: '```',
-  }
-
   return new Marp(opts).
-    use(require('markdown-it-plantuml'), plantUmlOptions).
+    use(require('markdown-it-textual-uml')).
     use(require('markdown-it-include'));
 }
