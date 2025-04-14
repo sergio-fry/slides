60e67e4b9bdad56e1584f9602d64d2b4cf71c769
Sergei O. Udalov <udalov.x@mail.ru>
2025-04-14 23:17:58 +0300

    patch_02 23:17

--- .engine/engine.js
+++ .engine/engine.js
@@ -4,6 +4,11 @@ const { Marp } = require('@marp-team/marp-core')
 // TODO: add code snippets lines
 // https://github.com/orgs/marp-team/discussions/164?ysclid=lovarg4cb969872172
 
+   //"@kazumatu981/markdown-it-kroki": "^1.3.4",
+   //"@marp-team/marp-cli": "^4.1.2",
+   //"@marp-team/marp-core": "^4.0.0",
+   //"markdown-it-include": "^2.0.0",
+   //"uri-js": "^4.4.1"
 module.exports = function(opts) {
   opts['html'] = true;
 
